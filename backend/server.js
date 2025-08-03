372204ffb707dac133916d3245dba3f6f39b5ea5
Harrison Katantazi <katantazi2025@gmail.com>
2025-08-04 01:37:06 +0200

    Implemented the delete option

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./config/db.js";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app = express();
 
-app.post("/products", async (req, res) => {
+app.use(express.json()) // allows to accept json data in the req.body
+
+app.get("/api/products", async (req, res) =>{
+    try {
+        const products = await product.find({});
+        res.status(200).json({ success: true, data: products});
+    } catch (error) {
+        res.status(500).json({ success: false, message: "Server Error"})
+
+    }
+})
+
+app.post("/api/products", async (req, res) => {
     const product = req.body; // User will send this data
 
     if(!product.name || product.price || product.image) {
@@ -23,6 +36,36 @@ app.post("/products", async (req, res) => {
         res.status(500).json({success: false, message: "server Error"});
     }
 });
+
+app.put("/api/products/:id", async (req, res) => {
+    const{ id } = req.params;
+
+    const product =req.body;
+
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({success:false, message:"invalid product ID"} );
+    }
+
+    try{
+        const updatedProduct = await product.findByIdAndUpdate(id, product,{new:true});
+        res.status(200).json({success:true, data: updatedProduct});
+
+    } catch(error) {
+        res.status.json({success: false, message: "server Error"});
+    }
+})
+
+app.delete("/api/products/:id", async (req, res) => {
+    const { id } = req.params;
+    
+    try {
+        await product.findByIdAndDelete(id);
+        res.status(200).json({success: true, message: "product deleted"});
+    }catch (error) {
+        console.log("Error in deleting the product:", error.message);
+        res.status(404).json({success: false, message: "product not found"});
+    }
+})
 
 app.listen(5000, () => {
     connectDB();
