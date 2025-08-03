20728804548177940106c1441fe73c6ab03d7f2b
Harrison Katantazi <katantazi2025@gmail.com>
2025-08-03 19:31:22 +0200

    Updated the DB

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
-import express from 'express';
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Server is ready")
-})
+app.get("/products", (req, res) => {})
+console.log (process.env.MONGO_URI)
 
 app.listen(5000, () => {
     console.log("Server started att http://localhost:5000" );
 });
+
+
