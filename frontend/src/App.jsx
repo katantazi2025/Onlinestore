658ecc6b016834e382e112594012410388c14993
Harrison <katantazi@2025.gmail.com>
2025-08-16 14:44:11 +0200

    several files added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,22 @@
-import { Button } from  "@chakra-ui/react";
+import { Box } from "@chakra-ui/react"
+import { Route, Routes } from "react-router-dom"
+import  CreatePage  from "./pages/CreatePage"
+import  HomePage  from "./pages/HomePage"
+import  Navbar  from "./Componets/Navbar"
+
 
 function App() {
-
-   return (
-    <>
-      
-        <button>Hello</button>
-       
-    </>
-  );
-
-}
   
+return (
+    <Box minH={"100vh"}>
+      { <Navbar /> }
+      <Routes>
+        <Route path="/" element={<HomePage/>} />
+        <Route path="/create" element={<CreatePage/>} />
+      </Routes>
+       
+    </Box>
+  )
+}
+
 export default App
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import  React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { Provider } from "@/components/ui/provider"
+import { ChakraProvider } from '@chakra-ui/react'
+import  { BrowserRouter } from  "react-router-dom"
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-     <Provider>
-      <App />
-    </Provider>
-  </StrictMode>,
+ <React.StrictMode>
+    <ChakraProvider>
+      <BrowserRouter>
+           <App />
+      </BrowserRouter>
+     </ChakraProvider>
+  </React.StrictMode>,
 )
